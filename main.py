477c59669de150b92abaaa687fcbaabbce358c09
oliverbravery <dev@oliverbravery.uk>
2023-05-28 16:38:30 +0100

    Added exception handling to all methods in DatabaseAPI class and also added functions check_price_difference and add_price.

--- main.py
+++ main.py
@@ -35,21 +35,32 @@ class TescoAPI:
 class DatabaseAPI:
     database_name = 'Tesco_Prices.db'
     def create_database(self):
-        connection = sqlite3.connect(self.database_name)
-        connection.execute('CREATE TABLE tblItems (tpnb INTEGER PRIMARY KEY, name TEXT);')
-        connection.execute(
-            'CREATE TABLE tblPrices (tpnb INTEGER, date_changed DATETIME, price DOUBLE, FOREIGN KEY (tpnb) REFERENCES tblItems (tpnb));')
-        connection.commit()
-        connection.close()
+        try:
+            if os.path.exists(self.database_name):
+                return 0
+            connection = sqlite3.connect(self.database_name)
+            connection.execute('CREATE TABLE tblItems (tpnb INTEGER PRIMARY KEY, name TEXT);')
+            connection.execute(
+                'CREATE TABLE tblPrices (tpnb INTEGER, date_changed DATETIME, price DOUBLE, FOREIGN KEY (tpnb) REFERENCES tblItems (tpnb));')
+            connection.commit()
+            connection.close()
+            return 0
+        except Exception:
+            print(f"Error creating the database: {Exception}")
+            return -1
 
     def get_items(self):
-        connection = sqlite3.connect(self.database_name)
-        cursor = connection.cursor()
-        cursor.execute('SELECT * FROM tblItems;')
-        results = cursor.fetchall()
-        json_data = json.dumps(results, indent=2)
-        connection.close()
-        return json_data
+        try:
+            connection = sqlite3.connect(self.database_name)
+            cursor = connection.cursor()
+            cursor.execute('SELECT * FROM tblItems;')
+            results = cursor.fetchall()
+            json_data = json.dumps(results, indent=2)
+            connection.close()
+            return json_data
+        except Exception:
+            print(f"Error getting item from database: {Exception}")
+            return -1
 
     def add_item(self, name, tpnb):
         try:
@@ -60,6 +71,35 @@ class DatabaseAPI:
             connection.close()
             return 0
         except Exception:
+            return -1
+
+    def check_price_difference(self, tpnb, price):
+        try:
+            connection = sqlite3.connect(self.database_name)
+            cursor = connection.cursor()
+            cursor.execute('SELECT price FROM tblPrices WHERE tpnb = ? ORDER BY date_changed DESC LIMIT 1;', (tpnb,))
+            results = cursor.fetchone()
+            if results is not None and results[0] != price:
+                return True
+            else:
+                return False
+            connection.close()
+        except Exception:
+            print(f"Error checking the price difference: {Exception}")
+            return -1
+
+    def add_price(self, tpnb, price):
+        try:
+            if self.check_price_difference(tpnb, price):
+                connection = sqlite3.connect(self.database_name)
+                cursor = connection.cursor()
+                cursor.execute('INSERT INTO tblPrices (tpnb, price, date_changed) VALUES (?, ?, CURRENT_TIMESTAMP);',
+                               (tpnb, price))
+                connection.commit()
+                connection.close()
+                return 0
+        except Exception:
+            print(f"Error adding a new price: {Exception}")
             return -1
 
 if __name__ == '__main__':
